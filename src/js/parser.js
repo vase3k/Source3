61f862bed1b812031a8cd63372105a54f9ff9fc7
Vasiliy VV <vase3k@gmail.com>
2024-04-05 11:57:24 +0300

    parser

---
+++ src/js/parser.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    let textNodes = [];
+
+    function recursy (element) {
+        element.childNodes.forEach(node => { 
+
+            if (node.nodeName.match(/^H/d)) {
+                const obj = {
+                    header: node.nodeName, 
+                    content: node.textContent
+                };
+                textNodes.push(obj);
+            } else {
+                recursy(node);
+            }
+        });
+    }
+
+    recursy(body);
+
+    fetch('https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(textNodes)
+        })
+        .then(response => response.json())
+        .then(json => console.log(json));
+        
+});      
+
